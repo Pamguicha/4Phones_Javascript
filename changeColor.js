f0af22f60c683857b9fc6143fa0b2ee25983a219
Yourfirstname Yourlastname <your-email@example.com>
2024-02-17 14:56:49 +1100

    functionality hover change color
    Event listener added to change colors hover with javaScript

--- changeColor.js
+++ changeColor.js
@@ -1,10 +1,10 @@
- //function mouseover and mouseout
+ //function mouseover and mouseout links menu bar
 
  const changeColorMenuBtn = document.querySelectorAll(".linksMenu");
 
  changeColorMenuBtn.forEach(color => {
   color.addEventListener("mouseover", function(){
-  color.style.backgroundColor = "pink";
+  color.style.backgroundColor = "#00d0b7";
   color.style.padding = "16px";
  });
 
@@ -14,3 +14,52 @@
  });
 
 });
+
+//function mouseover and mouseout title images 
+
+const changeLinksTitlesImages = document.querySelectorAll(".titleHomeProduct");
+
+changeLinksTitlesImages.forEach(link => {
+  link.addEventListener("mouseover", function(){
+  link.style.color = "#00d0b7";
+  });
+
+  link.addEventListener("mouseout", function(){
+  link.style.color = "#8c15e9";
+  });
+
+});
+
+//function mouseover and mouseout button checkoy
+
+const changeColorCheckoutBtn = document.querySelector(".checkout");
+
+changeColorCheckoutBtn.addEventListener("mouseover", function(){
+  changeColorCheckoutBtn.style.backgroundColor = "#8c15e9";
+});
+
+changeColorCheckoutBtn.addEventListener("mouseout", function(){
+  changeColorCheckoutBtn.style.backgroundColor = "#00d0b7";
+});
+
+//function btn register & clear form mouseover and mouseout
+const changeSubmitColor = document.querySelector(".submitRegisterBtn");
+
+changeSubmitColor.addEventListener("mouseover", function(){
+ changeSubmitColor.style.backgroundColor = "#00d0b7";
+});
+
+changeSubmitColor.addEventListener("mouseout", function(){
+  changeSubmitColor.style.backgroundColor = "#8c15e9";
+});
+
+const changeResetRf = document.querySelector("#resetBtnRf");
+changeResetRf.addEventListener("mouseover", function(){
+  changeResetRf.style.backgroundColor = "#8c15e9";
+});
+
+changeResetRf.addEventListener("mouseout", function(){
+  changeResetRf.style.backgroundColor = "#00d0b7";
+});
+
+
--- contactApp.js
+++ contactApp.js
@@ -87,3 +87,26 @@ function validateMessage(errorFound){
   }
 }
 
+//function change color mouse out and over btn submit and clear
+
+const changeSubmitContactColor = document.querySelector(".submitContactForm");
+
+changeSubmitContactColor.addEventListener("mouseover", function(){
+changeSubmitContactColor.style.backgroundColor = "#00d0b7";
+});
+
+changeSubmitContactColor.addEventListener("mouseout", function(){
+  changeSubmitContactColor.style.backgroundColor = "#8c15e9";
+});
+
+const changeClearContactBtn = document.querySelector(".clearForm");
+
+changeClearContactBtn.addEventListener("mouseover", function(){
+changeClearContactBtn.style.backgroundColor ="#8c15e9";
+});
+
+changeClearContactBtn.addEventListener("mouseout", function(){
+changeClearContactBtn.style.backgroundColor = "#00d0b7";
+});
+
+
